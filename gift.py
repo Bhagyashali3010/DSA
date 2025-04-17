7abdd376cedbb6b21b3e02779766f4ee52dadcdd
Bhagyashali Shinde <shindebhagyashali3010@gmail.com>
2025-04-17 09:16:52 +0530

    solved some more PS

--- gift.py
+++ gift.py
@@ -21,3 +21,40 @@ def count_valid_quadruples(x):
 
     return count
 
+
+#3
+# n=int(input("n:"))
+# arr=[None]*n
+# for i in range(n):
+#     arr[i]=int(input())
+# k=int(input("k:"))
+# a=arr[0]
+# mini=0
+# for i in range(1,k+1):
+#     if arr[i]<arr[mini]:
+#         mini= i
+#     if arr[i]==arr[mini]:
+#         mini=i
+# arr[0], arr[mini] = arr[mini], arr[0]
+# print(arr, a, mini)
+
+
+    
+#4
+n=int(input("n:"))
+arr=[None]*n
+for i in range(n):
+    arr[i]=int(input())
+c=0
+dicti={}
+for i in range(n):
+    dicti[arr[i]]=dicti.get(arr[i],0)+1
+    d=dict(sorted(dicti.items(), key=lambda item:item[1]))
+for i in (d.values()):
+    if c==0:
+        c+=i
+        a=2*i
+    else:
+        if i>=a:
+            c+=a
+print(c)
